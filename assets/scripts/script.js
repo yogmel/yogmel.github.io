40bf925e2ce0634f006ab0ace4ea0ae86386125c
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-05-13 00:14:04 -0300

    Add Prettier config file and format files. Add link to projects

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,52 @@
 const buttons = document.querySelectorAll(".experience__timeline > button");
-const activeBtn = document.querySelector(".experience__timeline > button.active");
-const experienceDescriptions = document.querySelectorAll('.experience__desc');
-const experiencePointer = document.getElementById('experiencePointer');
+const activeBtn = document.querySelector(
+  ".experience__timeline > button.active"
+);
+const experienceDescriptions = document.querySelectorAll(".experience__desc");
+const experiencePointer = document.getElementById("experiencePointer");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     resetStyles();
     changeHighlightBtn(button);
-  })
-})
+  });
+});
 
 const resetStyles = () => {
-  experienceDescriptions.forEach(expDesc => { expDesc.classList.remove('active') });
-  buttons.forEach(btn => { btn.classList.remove('active') });
-}
+  experienceDescriptions.forEach(expDesc => {
+    expDesc.classList.remove("active");
+  });
+  buttons.forEach(btn => {
+    btn.classList.remove("active");
+  });
+};
 
-const changeHighlightBtn = (button) => {
-  const dataAttribute = button.getAttribute('data-controls');
+const changeHighlightBtn = button => {
+  const dataAttribute = button.getAttribute("data-controls");
   const expDesc = document.getElementById(dataAttribute);
 
-  expDesc.classList.add('active');
+  expDesc.classList.add("active");
   button.classList.add("active");
 
   changePointerPosition(button);
-}
+};
 
 const changePointerPosition = elm => {
   if (window.innerWidth > 550) {
     experiencePointer.style.top = "4rem";
-    experiencePointer.style.left =
-      elm.offsetLeft +
-      elm.offsetWidth / 2;
+    experiencePointer.style.left = elm.offsetLeft + elm.offsetWidth / 2;
   } else {
     experiencePointer.style.left = "50%";
-    experiencePointer.style.top =
-      elm.offsetTop +
-      elm.offsetHeight;
+    experiencePointer.style.top = elm.offsetTop + elm.offsetHeight;
   }
-}
-
-window.onresize = function(){
-  changePointerPosition(document.querySelector(".experience__timeline > button.active"))
 };
 
-window.onload = () => { changePointerPosition(activeBtn) }
+window.onresize = function () {
+  changePointerPosition(
+    document.querySelector(".experience__timeline > button.active")
+  );
+};
+
+window.onload = () => {
+  changePointerPosition(activeBtn);
+};

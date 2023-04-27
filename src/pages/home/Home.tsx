2215dd49662b9f775571ee75fd4956596b15d58e
Mellina Y <yonashiro.mellina@gmail.com>
2023-04-27 11:52:25 +0200

    fix build

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,10 @@
-import About from "./About";
-import Education from "./Education";
-import Experience from "./Experience";
-import Footer from "./Footer";
-import Header from "./Header";
-import Projects from "./Projects";
-import Skills from "./Skills";
+import About from "./components/About";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function Home() {
   return (
